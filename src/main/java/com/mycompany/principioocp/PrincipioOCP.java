19d98bc4e5a457b7f4abacd39747731f49dac422
johan <jeramos61@ucatolica.edu.co>
2024-08-20 18:58:12 -0500

    Ejercicio sin modificacion
    ¿Cumple la clase Figuras el Principio OCP? 
    R: No cumple ya que el principio habla osbre que el codigo debe ser
    abierto a la extension cerrado a la modificacion si agregamos una
    nueva figura debemos modificar la clase figuras.

---
+++ src/main/java/com/mycompany/principioocp/PrincipioOCP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principioocp;
+
+/**
+ *
+ * @author johan
+ */
+public class PrincipioOCP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
