cf310c6b5e08e6ce856b29d6d240b9f19285c790
johan <jeramos61@ucatolica.edu.co>
2024-08-20 19:38:45 -0500

    Ejercicio modificado
    Si se considera refactorizacion debido a que se mejora el codigo 
    a nivel de rendimiento a la hora de querer actualizar 
    proceso ya que no necesitaros modifcar lo que ya existe

--- src/main/java/com/mycompany/principioocp/Figuras.java
+++ src/main/java/com/mycompany/principioocp/Figuras.java
@@ -13,26 +13,15 @@ import java.util.Vector;
  * @author johan
  */
 public class Figuras {
-Vector<Cuadrado> cuadrados=new Vector<Cuadrado>();
-Vector<Circulo> circulos= new Vector<Circulo>();
-public void addCirculo(Circulo c){
-circulos.add(c);
-}
-public void addCuadrado(Cuadrado c){
-cuadrados.add(c);
-}
-public void dibujarFiguras(){
-Enumeration<Cuadrado> cuads=cuadrados.elements();
-Cuadrado c;
-while (cuads.hasMoreElements()){
-c=cuads.nextElement();
-c.dibujar();
-}
-Enumeration<Circulo> circs=circulos.elements();
-Circulo ci;
-while (cuads.hasMoreElements()){
-ci=circs.nextElement();
-ci.dibujar();
-}
-}
+private List<Figura> figuras;
+
+    public void agregarFigura(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public void dibujarFiguras() {
+        for (Figura figura : figuras) {
+            figura.dibujar();
+        }
+    }
 }
